799ba24e02e22f1566425804fa92232a052593f6
Alex Günsche <mail@lxg.de>
2015-11-30 18:14:38 +0100

    migrate to new API endpoint/object registration

---
+++ Plugin/Api/SettingV1/Endpoint/Settings.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @package    agitation/settings
+ * @link       http://github.com/agitation/AgitSettingsBundle
+ * @author     Alex Günsche <http://www.agitsol.com/>
+ * @copyright  2012-2015 AGITsol GmbH
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\SettingBundle\Plugin\Api\SettingV1\Endpoint;
+
+use Agit\ApiBundle\Annotation\Endpoint;
+use Agit\ApiBundle\Common\AbstractEndpointClass;
+use Agit\PluggableBundle\Strategy\Depends;
+
+/**
+ * @Endpoint\EndpointClass
+ */
+class Settings extends AbstractEndpointClass
+{
+    /**
+     * @Endpoint\Endpoint(request="common.v1/String[]",response="Setting[]")
+     * @Endpoint\Security(capability="agit.settings.load")
+     * @Depends({"agit.settings"})
+     *
+     * Load application settings by setting names.
+     */
+    protected function load(array $names)
+    {
+        $result = [];
+        $settingList = $this->getService("agit.settings")->getSettings($names);
+
+        foreach ($settingList as $setting)
+            $result[] = $this->createObject("Setting", (object)[
+                "id" => $setting->getId(), "value" => $setting->getValue()
+            ]);
+
+        return $result;
+    }
+
+    /**
+     * @Endpoint\Endpoint(request="Setting[]",response="Setting[]")
+     * @Endpoint\Security(capability="agit.settings.save")
+     * @Depends({"agit.settings"})
+     *
+     * Save application settings.
+     */
+    protected function save(array $apiSettingList)
+    {
+        $settings = [];
+
+        foreach ($apiSettingList as $apiSetting)
+            $settings[$apiSetting->get("id")] = $apiSetting->get("value");
+
+        $settingList = $this->getService("agit.settings")->getSettings(array_keys($settings));
+
+        foreach ($settingList as $setting)
+            $setting->setValue($settings[$setting->getId()]);
+
+        $this->getService("agit.settings")->saveSettings($settingList);
+
+        return $this->load(array_keys($settings));
+    }
+}
--- Plugin/Api/SettingV1/Object/Setting.php
+++ Plugin/Api/SettingV1/Object/Setting.php
@@ -7,18 +7,21 @@
  * @license    http://opensource.org/licenses/MIT
  */
 
-namespace Agit\SettingBundle\Api\v1\Object;
+namespace Agit\SettingBundle\Plugin\Api\SettingV1\Object;
 
-use Agit\ApiBundle\Api\Object\AbstractObject;
-use Agit\ApiBundle\Api\Meta\Property;
+use Agit\ApiBundle\Annotation\Object;
+use Agit\ApiBundle\Annotation\Property;
+use Agit\ApiBundle\Common\AbstractObject;
 
 /**
+ * @Object\Object
+ *
  * An application setting.
  */
 class Setting extends AbstractObject
 {
     /**
-     * @Property\StringType(minLength=3,maxLength=40)
+     * @Property\StringType(minLength=3, maxLength=40)
      *
      * Identifier of the setting.
      */
