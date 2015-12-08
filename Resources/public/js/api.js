abea512d93c22045a91e97f3851d1156ad267f95
Alex Günsche <mail@lxg.de>
2015-12-09 00:10:05 +0100

    update API export

--- Resources/public/js/api.js
+++ Resources/public/js/api.js
@@ -1,22 +1,26 @@
 /*jslint white: true */
 /*global Agit */
 
-Agit.Endpoint.registerList({
-    "setting.v1/Settings.load": "common.v1/String[]",
-    "setting.v1/Settings.save": "setting.v1/Setting[]"
+Agit.Endpoint.register({
+    "setting.v1/Settings.load": [
+        "common.v1/String[]",
+        "setting.v1/Setting[]"
+    ],
+    "setting.v1/Settings.save": [
+        "setting.v1/Setting[]",
+        "setting.v1/Setting[]"
+    ]
 });
-Agit.Object.registerList({
+Agit.Object.register({
     "setting.v1/Setting": {
         "id": {
             "type": "string",
             "minLength": 3,
-            "maxLength": 40,
-            "name": "id"
+            "maxLength": 40
         },
         "value": {
             "type": "polymorphic",
-            "nullable": true,
-            "name": "value"
+            "nullable": true
         }
     }
 });
