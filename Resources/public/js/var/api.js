e5d35c6e11c9fbf2d3e825ec0c12ceb986f5b23d
Alex Günsche <mail@lxg.de>
2016-01-23 12:48:29 +0100

    fix paths for changed JS structure

---
+++ Resources/public/js/var/api.js
@@ -0,0 +1,23 @@
+agit.api.Endpoint.register({
+    "setting.v1/Settings.load": [
+        "common.v1/String[]",
+        "setting.v1/Setting[]"
+    ],
+    "setting.v1/Settings.save": [
+        "setting.v1/Setting[]",
+        "setting.v1/Setting[]"
+    ]
+});
+agit.api.Object.register({
+    "setting.v1/Setting": {
+        "id": {
+            "type": "string",
+            "minLength": 3,
+            "maxLength": 40
+        },
+        "value": {
+            "type": "polymorphic",
+            "nullable": true
+        }
+    }
+});
