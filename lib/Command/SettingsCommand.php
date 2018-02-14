64f9997bac23a63cf0279370be6272986b748347
Alexander Günsche <mail@lxg.de>
2018-02-14 12:48:41 +0100

    add command to modify settings through the CLI

---
+++ lib/Command/SettingsCommand.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Agit\SettingBundle\Command;
+
+use Exception;
+use RuntimeException;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SettingsCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('agit:settings')
+            ->setDescription('create or update settings')
+            ->addArgument('settings', InputArgument::REQUIRED, 'settings as JSON string');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $settings = json_decode($input->getArgument("settings"), true);
+
+        if (!is_array($settings))
+            throw new RuntimeException("A valid JSON object must be passed for settings.");
+
+        // the values themselves are validated by the settings service
+
+        $this->getContainer()->get("agit.setting")->saveSettings($settings);
+    }
+}
